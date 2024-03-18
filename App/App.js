0b603e30ea2046fed64fcf3b1026a49e91ce48eb
PavelPivkin <PavelP@mercaux.com>
2024-03-18 18:30:07 +0100

    Updates

--- App/App.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var Page_1 = require("../core/Page/Page");
-require("./App.sass");
-exports.App = function () {
-    return React.createElement(Page_1.Page, null);
-};
-//# sourceMappingURL=App.js.map
